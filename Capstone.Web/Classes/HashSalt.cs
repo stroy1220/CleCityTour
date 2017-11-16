4396fb799ea914838f3554a355b0a6ec4f8660c2
Teddilyn Gavozzi <tgavozzi@kent.edu>
2017-11-16 14:01:37 -0500

    created salt and hash

--- Capstone.Web/Classes/HashSalt.cs
+++ Capstone.Web/Classes/HashSalt.cs
@@ -11,35 +11,44 @@ namespace Capstone.Web.Classes
 {
     public class HashSalt
     {
-        static string HashPasswordWithMD5(string password)
+        //static string HashPasswordWithMD5(string password)
+        //{
+        //    MD5CryptoServiceProvider md5Provider = new MD5CryptoServiceProvider();
+
+        //    byte[] source = ASCIIEncoding.ASCII.GetBytes(password);
+        //    byte[] hash = md5Provider.ComputeHash(source);
+
+        //    return Convert.ToBase64String(hash);
+        //}
+
+        //static string HashPasswordWithPBKDF2(string password, byte[] salt, int workFactor)
+        //{
+        //    // Creates the crypto service provider and provides the salt - usually used to check for a password match
+        //    Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, salt, workFactor);
+
+        //    byte[] hash = rfc2898DeriveBytes.GetBytes(20);      //gets the hased password
+
+        //    return Convert.ToBase64String(salt) + "|" + Convert.ToBase64String(hash);
+        //}
+        
+
+        static Password HashPasswordWithPBKDF2(string password, int saltSize = 8, int workFactor = 1)
         {
-            MD5CryptoServiceProvider md5Provider = new MD5CryptoServiceProvider();
+            Password p = new Password();
+            string[] saltArray = new string[2];
 
-            byte[] source = ASCIIEncoding.ASCII.GetBytes(password);
-            byte[] hash = md5Provider.ComputeHash(source);
-
-            return Convert.ToBase64String(hash);
-        }
-
-        static string HashPasswordWithPBKDF2(string password, byte[] salt, int workFactor)
-        {
-            // Creates the crypto service provider and provides the salt - usually used to check for a password match
-            Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, salt, workFactor);
-
-            byte[] hash = rfc2898DeriveBytes.GetBytes(20);      //gets the hased password
-
-            return Convert.ToBase64String(salt) + "|" + Convert.ToBase64String(hash);
-        }
-
-        static string HashPasswordWithPBKDF2(string password, int saltSize, int workFactor)
-        {
             //Creates the crypto service provider and says to use a random salt of size "saltSize"
             Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, saltSize, workFactor);
 
             byte[] hash = rfc2898DeriveBytes.GetBytes(20);      //gets the hashed password
             byte[] salt = rfc2898DeriveBytes.Salt;              //gets the random salt
 
-            return Convert.ToBase64String(salt) + "|" + Convert.ToBase64String(hash);
+            string needsSplit = Convert.ToBase64String(salt) + "|" + Convert.ToBase64String(hash);
+            saltArray = needsSplit.Split('|');
+            p.UnSaltedPassword = saltArray[0];
+            p.Salt = saltArray[1];
+
+            return p;
         }
     }
 }
---
+++ Capstone.Web/Classes/Password.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Classes
+{
+    public class Password
+    {
+        public string UnSaltedPassword { get; set; }
+        public string Salt { get; set; }
+    }
+}
