3f628116147984625388c9b112834bb968464401
Samantha Troy <S.troy1220@gmail.com>
2017-11-16 13:32:32 -0500

    created db

--- database/data.sql
+++ database/data.sql
@@ -6,4 +6,7 @@ BEGIN;
 
 -- INSERT statements go here
 
+insert into userInfo (userName, firstName, lastName, password, admin) values('p.r@gmail.com', 'Pete', 'Rader', 'F0AE7F9D0BD3F50A1F9057895B0B5847', 0);
 COMMIT;
+
+select * from userInfo
--- database/schema.sql
+++ database/schema.sql
@@ -1,9 +1,59 @@
 -- *************************************************************************************************
 -- This script creates all of the database objects (tables, constraints, etc) for the database
 -- *************************************************************************************************
+--Drop table itinerary
+--Drop table itineraryPlaces
+--Drop table places
+--Drop table userInfo
 
-BEGIN;
+
+
+
+
 
 -- CREATE statements go here
+create table userInfo
+(
+id			 int		  primary key			identity(1,1),
+userName	 varchar(max) not null,
+firstName    varchar(max) not null,
+lastName     varchar(max) not null,
+password	 varchar(max) not null,
+passwordSalt varchar(max) null,
+admin		 bit	      not null,
 
-COMMIT;
+);
+
+create table places
+(
+id			  int		  primary key			identity(1,1),
+streetAddress varchar(max) not null,
+city          varchar(max) not null,
+state		  varchar(max) not null,
+latitude	  int          not null,
+longitude     int		   not null,
+googleID      varchar(max) null,
+detail        varchar(max) null,
+placeName     varchar(max) not null,
+);
+
+create table itinerary
+(
+id			  int		    primary key			identity(1,1),
+userID		  int		    not null,
+name		  varchar(max)  not null,
+startLocation varchar(max)  not null,
+date		  datetime		null,
+
+constraint fk_itinerary_userInfo foreign key (userID) references userInfo(id),
+)
+
+create table itineraryPlaces
+(
+id				  int		  primary key			identity(1,1),
+itineraryID       int         not null,		
+placeID			  int		  not null,
+
+constraint fk_itineraryPlaces_itinerary foreign key (itineraryID) references itinerary(id),
+constraint fk_itineraryPlaces_places foreign key (placeID) references places(id)
+);
