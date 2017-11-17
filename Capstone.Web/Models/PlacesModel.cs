9c9232aa0d14c4c58d722c04e647b7a6c9537074
Peter Rader <raderp841@gmail.com>
2017-11-17 13:30:41 -0500

    made placesModel and placesDal

---
+++ Capstone.Web/Models/PlacesModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Capstone.Web.Models
+{
+    public class PlacesModel
+    {
+        public int Id { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public int GoogleID { get; set; }
+        public string Detail { get; set; }
+        public string PlaceName { get; set; }
+        public string Category { get; set; }
+
+    }
+}
