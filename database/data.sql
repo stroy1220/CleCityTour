27708573c0d14d0d3fb8cdee261a6f8fa9545651
Samantha Troy <S.troy1220@gmail.com>
2017-11-17 14:40:16 -0500

    updated places

--- database/data.sql
+++ database/data.sql
@@ -9,4 +9,10 @@ BEGIN;
 insert into userInfo (userName, firstName, lastName, password, admin) values('p.r@gmail.com', 'Pete', 'Rader', 'F0AE7F9D0BD3F50A1F9057895B0B5847', 0);
 COMMIT;
 
-select * from userInfo;
+select * from places;
+delete from places where id = 2
+alter table places alter column latitude varchar(max) not null
+
+update places set category =  where id =16
+
+insert into places(streetAddress, city, state, latitude, longitude, detail, placeName, Category) values('700 W St Clair Ave', 'Cleveland', 'OH',41.49963580000001, -81.69923589999996, 'Bustling institution serving upscale seafood in an elegant, multilevel setting with skyline views', 'Blue Point Grill','Restaurant')
