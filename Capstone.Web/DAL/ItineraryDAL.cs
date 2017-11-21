ec71742ebab8ffefc929ff447cde45a43ceb5f9e
Samantha Troy <S.troy1220@gmail.com>
2017-11-21 15:24:14 -0500

    map now takes in user info and saves location

--- Capstone.Web/DAL/ItineraryDAL.cs
+++ Capstone.Web/DAL/ItineraryDAL.cs
@@ -48,183 +48,183 @@ namespace Capstone.Web.DAL
         }
 
         public bool CreateNewItinerary(ItineraryModel itinerary)
+    {
+        try
         {
-            try
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand(SQL_CreateNeweItinerary, conn);
-                    cmd.Parameters.AddWithValue("@userId", itinerary.UserId);
-                    cmd.Parameters.AddWithValue("@name", itinerary.Name);
-                    cmd.Parameters.AddWithValue("@startLocation", itinerary.StartLocation);
-                    cmd.Parameters.AddWithValue("@date", itinerary.Date);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(SQL_CreateNeweItinerary, conn);
+                cmd.Parameters.AddWithValue("@userId", itinerary.UserId);
+                cmd.Parameters.AddWithValue("@name", itinerary.Name);
+                cmd.Parameters.AddWithValue("@startLocation", itinerary.StartLocation);
+                cmd.Parameters.AddWithValue("@date", itinerary.Date);
 
 
-                    int rowsAffected = cmd.ExecuteNonQuery();
-                    return rowsAffected > 0;
-                }
-            }
-            catch (SqlException ex)
-            {
-                throw;
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
             }
         }
-
-        public bool DeleteItinerary(int id)
+        catch (SqlException ex)
         {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand(SQL_DeleteItinerary, conn);
-                    cmd.Parameters.AddWithValue("@id", id);
-
-                    int rowsAffected = cmd.ExecuteNonQuery();
-                    return rowsAffected > 0;
-                }
-            }
-            catch (SqlException ex)
-            {
-                throw;
-            }
-        }
-
-        public bool RemovePlaceFromItinerary(int id)
-        {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand(SQL_RemovePlaceFromItinerary, conn);
-                    cmd.Parameters.AddWithValue("@placeId", id);
-
-                    int rowsAffected = cmd.ExecuteNonQuery();
-                    return rowsAffected > 0;
-                }
-            }
-            catch (SqlException ex)
-            {
-                throw;
-            }
-        }
-
-        public List<PlacesModel> GetItinerarty(int id)
-        {
-            List<PlacesModel> output = new List<PlacesModel>();
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand(SQL_GetItinerary, conn);
-                    cmd.Parameters.AddWithValue("@id", id);
-                    SqlDataReader reader = cmd.ExecuteReader();
-
-                    while (reader.Read())
-                    {
-                        PlacesModel p = new PlacesModel();
-                        p.Category = Convert.ToString(reader["category"]);
-                        p.City = Convert.ToString(reader["city"]);
-                        p.StreetAddress = Convert.ToString(reader["streetAddress"]);
-                        p.State = Convert.ToString(reader["state"]);
-                        p.Latitude = Convert.ToDecimal(reader["latitude"]);
-                        p.Longitude = Convert.ToDecimal(reader["longitude"]);
-                        p.GoogleID = Convert.ToInt32(reader["googleID"]);
-                        p.Detail = Convert.ToString(reader["detail"]);
-                        p.PlaceName = Convert.ToString(reader["placeName"]);
-                        p.Zip = Convert.ToInt32(reader["zip"]);
-                        p.Id = Convert.ToInt32(reader["id"]);
-
-                        output.Add(p);
-                    }
-                }
-                return output;
-            }
-            catch (SqlException ex)
-            {
-                throw;
-            }
-        }
-
-        public bool UpdateName(string name, int id)
-        {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand(SQL_UpdateName, conn);
-                    cmd.Parameters.AddWithValue("@name", name);
-                    cmd.Parameters.AddWithValue("@id", id);
-
-                    int rowsAffected = cmd.ExecuteNonQuery();
-                    return rowsAffected > 0;
-                }
-            }
-            catch (SqlException ex)
-            {
-                throw;
-            }
-        }
-
-        public bool UpdateStartDate(DateTime startDate, int id)
-        {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand(SQL_StartDate, conn);
-                    cmd.Parameters.AddWithValue("@startDate", startDate);
-                    cmd.Parameters.AddWithValue("@id", id);
-
-                    int rowsAffected = cmd.ExecuteNonQuery();
-                    return rowsAffected > 0;
-                }
-            }
-            catch (SqlException ex)
-            {
-                throw;
-            }
-        }
-
-        public bool UpdateStartLocation(decimal lat, decimal longg, int id)
-        {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand(SQL_UpdateLocation, conn);
-                    cmd.Parameters.AddWithValue("@latitude", lat);
-                    cmd.Parameters.AddWithValue("@longitude", longg);
-                    cmd.Parameters.AddWithValue("@id", id);
-
-                    int rowsAffected = cmd.ExecuteNonQuery();
-                    return rowsAffected > 0;
-                }
-            }
-            catch (SqlException ex)
-            {
-                throw;
-            }
-        }
-        public List<ItineraryModel> GetAllItinerary(int userId)
-        {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(connectionString))
-                {
-
-                }
-            }
-            catch(SqlException ex)
-            {
-                throw;
-            }
+            throw;
         }
     }
+
+    public bool DeleteItinerary(int id)
+    {
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(SQL_DeleteItinerary, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+        catch (SqlException ex)
+        {
+            throw;
+        }
+    }
+
+    public bool RemovePlaceFromItinerary(int id)
+    {
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(SQL_RemovePlaceFromItinerary, conn);
+                cmd.Parameters.AddWithValue("@placeId", id);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+        catch (SqlException ex)
+        {
+            throw;
+        }
+    }
+
+    public List<PlacesModel> GetItinerarty(int id)
+    {
+        List<PlacesModel> output = new List<PlacesModel>();
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(SQL_GetItinerary, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    PlacesModel p = new PlacesModel();
+                    p.Category = Convert.ToString(reader["category"]);
+                    p.City = Convert.ToString(reader["city"]);
+                    p.StreetAddress = Convert.ToString(reader["streetAddress"]);
+                    p.State = Convert.ToString(reader["state"]);
+                    p.Latitude = Convert.ToDecimal(reader["latitude"]);
+                    p.Longitude = Convert.ToDecimal(reader["longitude"]);
+                    p.GoogleID = Convert.ToInt32(reader["googleID"]);
+                    p.Detail = Convert.ToString(reader["detail"]);
+                    p.PlaceName = Convert.ToString(reader["placeName"]);
+                    p.Zip = Convert.ToInt32(reader["zip"]);
+                    p.Id = Convert.ToInt32(reader["id"]);
+
+                    output.Add(p);
+                }
+            }
+            return output;
+        }
+        catch (SqlException ex)
+        {
+            throw;
+        }
+    }
+
+    public bool UpdateName(string name, int id)
+    {
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(SQL_UpdateName, conn);
+                cmd.Parameters.AddWithValue("@name", name);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+        catch (SqlException ex)
+        {
+            throw;
+        }
+    }
+
+    public bool UpdateStartDate(DateTime startDate, int id)
+    {
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(SQL_StartDate, conn);
+                cmd.Parameters.AddWithValue("@startDate", startDate);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+        catch (SqlException ex)
+        {
+            throw;
+        }
+    }
+
+    public bool UpdateStartLocation(decimal lat, decimal longg, int id)
+    {
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(SQL_UpdateLocation, conn);
+                cmd.Parameters.AddWithValue("@latitude", lat);
+                cmd.Parameters.AddWithValue("@longitude", longg);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+        catch (SqlException ex)
+        {
+            throw;
+        }
+    }
+    public List<ItineraryModel> GetAllItinerary(int userId)
+    {
+        try
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+
+            }
+        }
+        catch (SqlException ex)
+        {
+            throw;
+        }
+    }
+}
 }
