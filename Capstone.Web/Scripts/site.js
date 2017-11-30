394f1822b8e526263ef6b980de8752f2c62dc80e
Samantha Troy <S.troy1220@gmail.com>
2017-11-30 11:10:18 -0500

    moved index map to js file

--- Capstone.Web/Scripts/site.js
+++ Capstone.Web/Scripts/site.js
@@ -4,18 +4,12 @@ var markers = [];
 
 function loadMap() {
 
-    //var baseUrl = ""; http://localhost:52923/ samURL
-
-
-    
     var uniqueId = 1;
-
 
     var boxItin = $("#itinModal");
     boxItin.hide();
 
     var wholeThing = $('.wholePage');
-    
 
     $("#sortable").sortable();
     $(".sortableItems").sortable({
@@ -55,8 +49,6 @@ function loadMap() {
         });
     });
 
-
-
     var icons = {
         header: "ui-icon-circle-arrow-e",
         activeHeader: "ui-icon-circle-arrow-s"
@@ -68,14 +60,12 @@ function loadMap() {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
-
     $(".accordion").accordion({
         header: "h3",
         heightStyle: "content",
         icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" },
     });
     $(".itinName.ui-accordion-header").css("background", "maroon").css("height", "50px").css("color", "white").css("font-size", "25px");
-
 
     var map = new google.maps.Map(document.getElementById("dvMap"), mapOptions);
 
@@ -94,8 +84,6 @@ function loadMap() {
         //Set unique id
         marker.id = uniqueId;
         uniqueId++;
-
-
 
         //Attach click event handler to the marker.
         google.maps.event.addListener(marker, "click", function (e) {
@@ -117,9 +105,7 @@ function loadMap() {
         //Add marker to the array.
         markers.push(marker);
     });
-
 }
-
 
 function DeleteMarker(id) {
     //Find and remove the marker from the Array
@@ -134,7 +120,6 @@ function DeleteMarker(id) {
         }
     }
 };
-
 
 function SaveMarker(id, lat, lng) {
     var name = $(`input[data-id=name-${id}]`).val();
@@ -162,8 +147,6 @@ function SaveMarker(id, lat, lng) {
             var zip = $("#zip").val();
             var fullAddress = $("#fullAddress").val();
             var total = $("#total").val();
-
-
 
             $.each(components, function (index, component) {
                 if (component.types[0] === 'locality') {
@@ -208,5 +191,82 @@ function SaveMarker(id, lat, lng) {
 
         });
     });
-
 }
+
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+
+function initialize() {
+
+    var markers = [];
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(41.495820, -81.702458),
+        new google.maps.LatLng(41.499320, -81.694361)
+    );
+    map.fitBounds(defaultBounds);
+
+    //Create the search box and link it to the UI element.*/
+    var input =/* @*@type {HTMLInputElement}*@*/ (
+        document.getElementById('pac-input'));
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+
+    var searchBox = new google.maps.places.SearchBox(
+             /*@*type {HTMLInputElement}*@*/(input));
+
+    // [START region_getplaces]
+    // Listen for the event fired when the user selects an item from the
+    // pick list. Retrieve the matching places for that item.
+    google.maps.event.addListener(searchBox, 'places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+        for (var i = 0, marker; marker = markers[i]; i++) {
+            marker.setMap(null);
+        }
+
+        // For each place, get the icon, place name, and location.
+        markers = [];
+        var bounds = new google.maps.LatLngBounds();
+        for (var i = 0, place; place = places[i]; i++) {
+            var image = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            var marker = new google.maps.Marker({
+                map: map,
+                icon: image,
+                title: place.name,
+                position: place.geometry.location
+            });
+
+            markers.push(marker);
+
+            bounds.extend(place.geometry.location);
+        }
+
+        map.fitBounds(bounds);
+    });
+    // [END region_getplaces]
+
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function () {
+        var bounds = map.getBounds();
+        searchBox.setBounds(bounds);
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
